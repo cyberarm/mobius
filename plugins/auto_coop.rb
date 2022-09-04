cd09e56575b3e8c4b2f3b69d57efefe3491e1851
Cyberarm <matthewlikesrobots@gmail.com>
2022-09-04 10:14:30 -0500

    Update auto coop to force coop team to allied on seamist

--- plugins/auto_coop.rb
+++ plugins/auto_coop.rb
@@ -47,28 +47,47 @@ mobius_plugin(name: "AutoCoop", version: "0.0.1") do
     RenRem.cmd("player_info")
   end
 
+  def check_map(map)
+    case map
+    when "RA_AS_Seamist", "RA_AS_Seamist.mix"
+      @current_side = 1
+    end
+  end
+
   on(:start) do
     @current_side = 0
     @bot_difficulty = 2
     @support_bots = 4
     @last_bot_count = -1
 
+    @coop_started = false
+
     every(5) do
-      configure_bots
-      move_players_to_coop_team
+      if @coop_started
+        configure_bots
+        move_players_to_coop_team
+      end
     end
   end
 
-  on(:map_loaded) do
+  on(:map_loaded) do |map|
     @current_side += 1
     @current_side %= 2
 
+    check_map(map)
+
     after(5) do
-      count = configure_bots
+      if PlayerData.player_list.size.positive?
+        @coop_started = true
 
-      broadcast_message("[AutoCoop] Starting coop on team #{Teams.name(@current_side)} with #{count / 2} bots per team")
+        count = configure_bots
 
-      move_players_to_coop_team
+        broadcast_message("[AutoCoop] Starting coop on team #{Teams.name(@current_side)} with #{count / 2} bots per team")
+
+        move_players_to_coop_team
+      else
+        @coop_started = false
+      end
     end
   end
 
