312da5498497ab80ff010cf5c15073298995100e
Cyberarm <matthewlikesrobots@gmail.com>
2023-08-14 16:56:37 +0000

    Revert "Preserve case of map name"
    This reverts commit 576792aab7892dfc103302f369a75369f2cd7e70.

--- plugins/auto_announce.rb
+++ plugins/auto_announce.rb
@@ -24,6 +24,7 @@ mobius_plugin(name: "AutoAnnounce", database_name: "auto_announce", version: "0.
     @sayings = [
       "This server is running Mobius v#{Mobius::VERSION}",
       "Remember to join voice chat on Discord! (https://discord.gg/jMmmRa2)",
+      "APB with 100% more coop!",
       "Report issues or concerns to @cyberarm on the W3D Hub discord server",
       proc { "The server time is: #{Time.now.utc.strftime('%Y-%m-%d %H:%M:%S')} UTC" }
     ]
