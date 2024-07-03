1314d37e725a3848e767a24cab46f330c920d104
Cyberarm <matthewlikesrobots@gmail.com>
2024-07-03 17:51:32 -0500

    Added RetiredNicknames plugin

---
+++ plugins/retired_nicknames.rb
@@ -0,0 +1,8 @@
+mobius_plugin(name: "RetiredNicknames", database_name: "retired_nicknames", version: "0.0.1") do
+  on(:player_joined) do |player|
+    if (config[:retired_nicknames] || []).find { |nickname| player.name.downcase.strip == nickname.downcase.strip }
+      kick_player!(player, "This nickname is has been retired, please choose another nickname.")
+      broadcast_message("[MOBIUS] Player using #{player.name}'s nickname has been kicked. Nickname has been retired.", red: 179, green: 25, blue: 66)
+    end
+  end
+end
